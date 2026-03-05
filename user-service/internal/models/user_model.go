32d846055bf1428f1bdea1318e645e522a185e40
Hernan Sarmiento <hernansr8@gmail.com>
2026-03-05 02:05:20 -0300

    fix: added user model

---
+++ user-service/internal/models/user_model.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	UserID   uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Username string    `gorm:"type:varchar(50);not null"`
+	Email    string    `gorm:"type:varchar(50);not null"`
+	Password string    `gorm:"type:varchar(50);not null"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	if u.UserID == uuid.Nil {
+		u.UserID = uuid.New()
+	}
+	return nil
+}
