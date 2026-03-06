05c3da2af2402a4f25e8ad6060b6ddedfc1dcfd4
Hernan Sarmiento <hernansr8@gmail.com>
2026-03-06 16:03:41 -0300

    fix: fixed password length capacity

--- user-service/internal/models/user_model.go
+++ user-service/internal/models/user_model.go
@@ -9,7 +9,7 @@ type User struct {
 	UserID   uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Username string    `gorm:"type:varchar(50);not null"`
 	Email    string    `gorm:"type:varchar(50);not null"`
-	Password string    `gorm:"type:varchar(50);not null"`
+	Password string    `gorm:"type:varchar(100);not null"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
