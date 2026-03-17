e1b9c5dd71a87b620ba9ab80f80ceb553004eaee
Hernan Sarmiento <hernansr8@gmail.com>
2026-03-17 14:31:52 -0300

    feat: added role field to user_model to implement jwt in auth-service

--- user-service/internal/models/user_model.go
+++ user-service/internal/models/user_model.go
@@ -5,11 +5,19 @@ import (
 	"gorm.io/gorm"
 )
 
+type Role string
+
+const (
+	RoleAdmin Role = "admin"
+	RoleUser  Role = "user"
+)
+
 type User struct {
 	UserID   uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Username string    `gorm:"type:varchar(50);not null"`
 	Email    string    `gorm:"type:varchar(50);not null"`
 	Password string    `gorm:"type:varchar(100);not null"`
+	Role     Role      `gorm:"type:varchar(20);not null; default:'user'"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
