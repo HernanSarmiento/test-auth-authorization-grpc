618e28e9c840ba11224aadd46e274adbef4f18f9
Hernan Sarmiento <hernansr8@gmail.com>
2026-03-18 18:38:47 -0300

    feat: added private & public reference to config struct

--- auth-service/internal/config/config.go
+++ auth-service/internal/config/config.go
@@ -3,13 +3,15 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	DB_HOST     string `mapstructure:"DB_HOST"`
-	DB_USER     string `mapstructure:"DB_USER"`
-	DB_PASSWORD string `mapstructure:"DB_PASSWORD"`
-	DB_NAME     string `mapstructure:"DB_NAME"`
-	DB_SSLMODE  string `mapstructure:"DB_SSLMODE"`
-	DB_PORT     string `mapstructure:"DB_PORT"`
-	SERVER_PORT string `mapstructure:"SERVER_PORT"`
+	DB_HOST        string `mapstructure:"DB_HOST"`
+	DB_USER        string `mapstructure:"DB_USER"`
+	DB_PASSWORD    string `mapstructure:"DB_PASSWORD"`
+	DB_NAME        string `mapstructure:"DB_NAME"`
+	DB_SSLMODE     string `mapstructure:"DB_SSLMODE"`
+	DB_PORT        string `mapstructure:"DB_PORT"`
+	SERVER_PORT    string `mapstructure:"SERVER_PORT"`
+	PrivateKeyPath string `mapstructure:"PRIVATE_KEY_PATH"`
+	PublicKeyPath  string `mapstructure:"PUBLIC_KEY_PATH"`
 }
 
 func LoadConfig() (cfg Config, err error) {
