709c35ddf2527fce41b4979501f39786aac797a4
Hernan Sarmiento <hernansr8@gmail.com>
2026-03-28 17:18:21 -0300

    feat: added authorid & author_name field in post gorm model

--- blog-service/internal/models/post_model.go
+++ blog-service/internal/models/post_model.go
@@ -8,12 +8,14 @@ import (
 )
 
 type Post struct {
-	PostID    uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	Title     string    `gorm:"type:varchar(100);not null"`
-	Body      string    `gorm:"type:text;not null"`
-	CreatedAt time.Time `gorm:""`
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	PostID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Title      string    `gorm:"type:varchar(100);not null"`
+	Body       string    `gorm:"type:text;not null"`
+	AuthorID   string    `gorm:"type:index;not null"`
+	AuthorName string    `gorm:"type:varchar(50);;not null"`
+	CreatedAt  time.Time `gorm:""`
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
 }
 
 func (p *Post) BeforeCreate(tx *gorm.DB) (err error) {
