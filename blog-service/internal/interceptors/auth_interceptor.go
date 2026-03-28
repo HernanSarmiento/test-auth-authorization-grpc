92a0a3500d3230da5e8d624759eec012b6bac741
Hernan Sarmiento <hernansr8@gmail.com>
2026-03-28 19:13:05 -0300

    feat: added white list to auth_interceptor

--- blog-service/internal/interceptors/auth_interceptor.go
+++ blog-service/internal/interceptors/auth_interceptor.go
@@ -3,22 +3,46 @@ package interceptors
 import (
 	"context"
 	"crypto/ecdsa"
+	"fmt"
 
 	"github.com/HernanSarmiento/test-auth-authorization-grpc/common/auth"
 	"google.golang.org/grpc"
 )
 
+var publicMethods = map[string]bool{
+	"/blog-service/GetAllPosts": true,
+	"/blog-service/GetPost":     true,
+}
+
 func AuthInterceptor(pubKey *ecdsa.PublicKey) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+
+		fmt.Printf("[Interceptor]Trying to access to: %s\n", info.FullMethod)
+
+		if publicMethods[info.FullMethod] {
+			return handler(ctx, req)
+		}
 
 		claims, err := auth.VerifyToken(ctx, pubKey)
 		if err != nil {
 			return nil, err
 		}
 
-		newctx := context.WithValue(ctx, auth.UserIDKey, claims.ID)
-		newCtx := context.WithValue(newctx, auth.RoleKey, auth.AuthorNameKey)
+		fmt.Printf("[Interceptor]: User %s (%s) authorized for %s\n",
+			claims.AuthorName, claims.ID, info.FullMethod,
+		)
 
-		return handler(newCtx, req)
+		ctx = context.WithValue(ctx, auth.UserIDKey, claims.UserID)
+		ctx = context.WithValue(ctx, auth.AuthorNameKey, claims.AuthorName)
+		ctx = context.WithValue(ctx, auth.RoleKey, claims.Role)
+
+		resp, err := handler(ctx, req)
+		if err != nil {
+			fmt.Printf("[Interceptor]: Method %s executed by %s FAILED: %s\n", info.FullMethod, claims.AuthorName, err)
+		} else {
+			fmt.Printf("[Interceptor]: Method %s executed Successfully by %s SUCCESS", info.FullMethod, claims.AuthorName)
+		}
+
+		return resp, err
 	}
 }
