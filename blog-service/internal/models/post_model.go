f0513170b9d83287946a0b99cc2ba87b45ae3bb4
Hernan Sarmiento <hernansr8@gmail.com>
2026-03-31 15:24:59 -0300

    fix:

--- blog-service/internal/models/post_model.go
+++ blog-service/internal/models/post_model.go
@@ -11,7 +11,7 @@ type Post struct {
 	PostID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Title      string    `gorm:"type:varchar(100);not null"`
 	Body       string    `gorm:"type:text;not null"`
-	AuthorID   string    `gorm:"type:index;not null"`
+	AuthorID   string    `gorm:"type:varchar(255);not null"`
 	AuthorName string    `gorm:"type:varchar(50);;not null"`
 	CreatedAt  time.Time `gorm:""`
 	UpdatedAt  time.Time
